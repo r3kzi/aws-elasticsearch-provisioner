fc02035231d6cd2ccb37ccb72f12681f388bc0bd
Christian Niehoff <christian.niehoff@dwpbank.de>
2020-06-08 17:09:56 +0200

    add http test

--- pkg/http/http_test.go
+++ pkg/http/http_test.go
@@ -1,15 +1,27 @@
 package http
 
 import (
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"testing"
 )
 
 const (
-	url  = "https://elasticsearch"
-	body = "body"
+	url     = "https://elasticsearch"
+	body    = "body"
+	service = "es"
+	region  = "eu-west-1"
 )
+
+type CredentialsTestProvider struct{}
+
+func (c *CredentialsTestProvider) Retrieve() (credentials.Value, error) {
+	return credentials.Value{}, nil
+}
+func (c *CredentialsTestProvider) IsExpired() bool {
+	return false
+}
 
 func TestNewRequest(t *testing.T) {
 	request, err := NewRequest(url, body)
@@ -20,4 +32,15 @@ func TestNewRequest(t *testing.T) {
 	bytes, err := ioutil.ReadAll(request.Body)
 	assert.Nil(t, err)
 	assert.Equal(t, string(bytes), "body")
+}
+
+func TestSignRequest(t *testing.T) {
+	creds := credentials.NewCredentials(&CredentialsTestProvider{})
+	request, err := NewRequest(url, body)
+	assert.Nil(t, err)
+
+	signRequest, err := SignRequest(request, body, creds, service, region)
+	assert.Nil(t, err)
+	assert.NotNil(t, signRequest)
+	assert.NotEmpty(t, signRequest.Header.Get("Authorization"))
 }
