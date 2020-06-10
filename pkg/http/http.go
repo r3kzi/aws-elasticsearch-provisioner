83915a3f3ff42b0871ddaa5099103b594cc7c5d0
Christian Niehoff <christian.niehoff@dwpbank.de>
2020-06-10 16:22:03 +0200

    rewrite

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -3,7 +3,8 @@ package http
 import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws/credentials"
-	v4 "github.com/aws/aws-sdk-go/aws/signer/v4"
+	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
@@ -13,7 +14,7 @@ import (
 func NewRequest(url string, body string) (*http.Request, error) {
 	req, err := http.NewRequest(http.MethodPut, url, strings.NewReader(body))
 	if err != nil {
-		return nil, fmt.Errorf(fmt.Sprintf("Error creating request, %s", err))
+		return nil, fmt.Errorf("error creating http request, %s", err)
 	}
 	req.Header.Add("Content-Type", "application/json")
 	return req, nil
@@ -25,17 +26,22 @@ func SignRequest(req *http.Request, body string, creds *credentials.Credentials,
 	signer := v4.NewSigner(creds)
 	_, err := signer.Sign(req, strings.NewReader(body), service, region, time.Now())
 	if err != nil {
-		return nil, fmt.Errorf(fmt.Sprintf("Error signing request, %s", err))
+		return nil, fmt.Errorf("error signing http request, %s", err)
 	}
 	return req, nil
 }
 
 // DoRequest will actually make the http call
-func DoRequest(req *http.Request) (*http.Response, error) {
+func DoRequest(req *http.Request) error {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, fmt.Errorf(fmt.Sprintf("Error making api call, %s", err))
+		return fmt.Errorf("error making elasticsearch api call: %s", err)
 	}
-	return resp, nil
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("returned code was %s - message was %s", resp.Status, string(bytes))
+	}
+	return nil
 }
--- pkg/user/user.go
+++ pkg/user/user.go
@@ -6,72 +6,37 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/r3kzi/elasticsearch-provisioner/pkg/cfg"
 	"github.com/r3kzi/elasticsearch-provisioner/pkg/http"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 )
 
 const userAPIEndpoint = "_opendistro/_security/api/internalusers"
 
-// User correspond a Kibana User
-type User struct {
-	Password     string   `yaml:"password" json:"password"`
-	BackendRoles []string `yaml:"backend_roles" json:"backend_roles"`
-}
-
-// Read will a file und tries to parse User structs
-func Read(filename string) (map[string]User, error) {
-	file, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, fmt.Errorf(fmt.Sprintf("Error reading users file, %s", err))
-	}
-
-	var users map[string]User
-	if err := yaml.Unmarshal(file, &users); err != nil {
-		return nil, fmt.Errorf(fmt.Sprintf("Error unmarshalling users file, %s", err))
-	}
-	return users, nil
-}
-
 // Create will create all User using the http package
-func Create(users map[string]User, config *cfg.Config, creds *credentials.Credentials) error {
-	for key, value := range users {
-		jsonUser, err := json.Marshal(value)
-		if err != nil {
-			return fmt.Errorf(fmt.Sprintf("For user: %s, %s", key, err))
-		}
+func Create(config *cfg.Config, creds *credentials.Credentials) error {
+	for _, user := range config.Users {
+		for key, value := range user {
+			jsonUser, err := json.Marshal(value)
+			if err != nil {
+				return fmt.Errorf("for user: %s - %s", key, err)
+			}
 
-		url := fmt.Sprintf("%s/%s/%s", config.Elasticsearch.Endpoint, userAPIEndpoint, key)
-		body := string(jsonUser)
+			url := fmt.Sprintf("%s/%s/%s", config.Elasticsearch.Endpoint, userAPIEndpoint, key)
+			body := string(jsonUser)
 
-		request, err := http.NewRequest(url, string(jsonUser))
-		if err != nil {
-			return fmt.Errorf(fmt.Sprintf("For user: %s, %s", key, err))
-		}
+			request, err := http.NewRequest(url, string(jsonUser))
+			if err != nil {
+				return fmt.Errorf("for user: %s - %s", key, err)
+			}
 
-		signRequest, err := http.SignRequest(request, body, creds, "es", config.AWS.Region)
-		if err != nil {
-			return fmt.Errorf(fmt.Sprintf("For user: %s, %s", key, err))
-		}
+			signRequest, err := http.SignRequest(request, body, creds, "es", config.AWS.Region)
+			if err != nil {
+				return fmt.Errorf("for user: %s - %s", key, err)
+			}
 
-		resp, err := http.DoRequest(signRequest)
-		if err != nil {
-			return fmt.Errorf(fmt.Sprintf("For user: %s, %s", key, err))
-		}
-
-		if resp.StatusCode != 200 && resp.StatusCode != 201 {
-			bodyBytes, _ := ioutil.ReadAll(resp.Body)
-			bodyString := string(bodyBytes)
-			return fmt.Errorf(fmt.Sprintf("Failed creating user: %s, Status Code was %d\n"+
-				"Message: %s", key, resp.StatusCode, bodyString))
+			err = http.DoRequest(signRequest)
+			if err != nil {
+				return fmt.Errorf("for user: %s - %s", key, err)
+			}
 		}
 	}
 	return nil
-}
-
-// Must ensures validate user map
-func Must(users map[string]User, err error) map[string]User {
-	if err != nil {
-		panic(err)
-	}
-	return users
 }
